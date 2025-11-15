cc967d73600a1f88fd8feb8861605ff82eb0758e
nev-nightmare <nevaav.jain@gmail.com>
2025-11-15 17:55:44 +0530

    Update 1 judgeresult.py

--- judgeresult.py
+++ judgeresult.py
@@ -25,28 +25,28 @@ for i in range(test_count):
     elif test_expect[i]==test_actual[i] and time_taken[i]>=2 and memory[i]<256:
         print(f'Testcase {i}: Time limit exceeded')
         print(f'Expected Outcome: {test_expect[i]}\nActual Outcome: {test_actual[i]}')
-        success=-1
+        success=-1            #notes limit problem
         loc=i
         break
     elif test_expect[i]==test_actual[i] and time_taken[i]<2 and memory[i]>=256:
         print(f'Testcase {i}: Memory limit exceeded')
         print(f'Expected Outcome: {test_expect[i]}\nActual Outcome: {test_actual[i]}')
-        success=-1
+        success=-1            #notes limit problem
         loc=i
         break
     elif test_expect[i]==test_actual[i] and time_taken[i]>=2 and memory[i]>=256:
         print(f'Testcase {i}: Time limit and Memory limit exceeded')
         print(f'Expected Outcome: {test_expect[i]}\nActual Outcome: {test_actual[i]}')
-        success=-1
+        success=-1            #notes limit problem
         loc=i
         break
     elif test_expect[i]!=test_actual[i]:
         print(f'Testcase {i}: Time limit and Memory limit exceeded')
         print(f'Expected Outcome: {test_expect[i]}\nActual Outcome: {test_actual[i]}')
-        success=-2
+        success=-2            #notes mismatch
         loc=i
         break
 print('\nOverall:',end=' ')
 elif success==test_count: print('Accepted')
-elif success==-1: print('Limits exceeded')
-elif success==-2: print('Output mismatch')
+elif success==-1: print(f'Limits exceeded at {loc+1}')
+elif success==-2: print(f'Output mismatch at {loc+1}')
