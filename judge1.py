14ef49c3a95cf6b7952a6102c0d344de2f977384
kedarrrr-m <kedarmallya0@gmail.com>
2025-11-25 23:18:10 +0530

    Add files via upload

--- judge1.py
+++ judge1.py
@@ -4,10 +4,10 @@ import subprocess
 import os
 
 class Judge:
-    def __init__(self, tl=2 ):
+    def __init__(self, tl=3):
         self.time_limit = tl
 
-    def compile_code(self, path, language):
+    def compile_code(self, path, language):#returns a list of commands to execute in cmd
         if language == "python":
             return path
         output_file = path + "_out"
@@ -15,9 +15,7 @@ class Judge:
             cmd = ["gcc", path, "-o", output_file]
         elif language == "cpp":
             cmd = ["g++", path, "-o", output_file]
-        elif language == "java":
-            cmd = ["javac", path]
-            output_file = path.replace(".java", "")
+
         try:
             subprocess.run(cmd, capture_output=True, timeout=self.time_limit)
             return output_file
@@ -25,60 +23,35 @@ class Judge:
             print("Compilation error")
             return None
 
-
     def run(self, exe_path, input_data, language):
         try:
             if language == "python":
                 cmd = ["python3", exe_path]
-
             elif language in ("c", "cpp"):
                 cmd = [exe_path]
-
-            result = subprocess.run(
-                cmd,
-                input=input_data.encode(),
-                capture_output=True,
-                timeout=self.time_limit
-            )
-
-            return result.stdout.decode().strip(), None
-
+            result = subprocess.run(cmd,input=input_data.encode("utf-8"),capture_output=True,timeout=self.time_limit)
+            return result.stdout.decode("utf-8").strip(), None
         except subprocess.TimeoutExpired:
-            print("Time Limit Excedeeded")
+            print("Time Limit Exceeded")
             return None, "TLE"
-
-        except Exception as err:
-            return None, "The error is " +str(err)
-
+        except Exception as error:
+            return None, "The error is " +str(error)
 
     def judge(self, path, language, testcases):#testcases is a list of tuples where first is input string and second is output string
         exe = self.compile_code(path, language)
         if exe is None:
             return {"status": "COMPILE_ERROR"}
-
         results = []
-        all_passed = True
-
-        for inp, exp in testcases:
-            out, err = self.run(exe, inp, language)
-
-            if err is not None:
-                results.append({"input": inp, "status": err})
-                all_passed = False
+        all_have_passed = True
+        for input, expected in testcases:
+            output, error = self.run(exe, input, language)
+            if error is not None:
+                results.append({"input": input, "status": error})
+                all_have_passed = False
                 continue
-
-            if out == exp.strip():
-                results.append({"input": inp, "status": "PASS"})
+            if output == expected.strip():
+                results.append({"input": input, "status": "PASS"})
             else:
-                results.append({
-                    "input": inp,
-                    "status": "FAIL",
-                    "got": out,
-                    "expected": exp
-                })
-                all_passed = False
-
-        return {
-            "overall": "PASS" if all_passed else "FAIL",
-            "details": results
-        }
+                results.append({"input": input,"status": "FAIL","got": output,"expected": expected})
+                all_have_passed = False
+        return {"overall": "PASS" if all_have_passed else "FAIL","details": results}
--- submissions.py
+++ submissions.py
@@ -1,6 +1,6 @@
 import os
 class submissions:
-  def __init__(self,sid,uid,pid,lang,code,tstamp):
+  def __init__(self,sid=None,uid=None,pid=None,lang=None,code=None,tstamp=None):
      #constructor and attributes
      
 # changes in validation 
@@ -18,8 +18,6 @@ class submissions:
     if not isinstance(tstamp, (int, float)) or tstamp < 0:
         raise ValueError("Timestamp must be a non-negative number")
     
-
-
     self.submission_id=sid
     self.user_id = uid
     self.problem_id = pid
@@ -28,20 +26,17 @@ class submissions:
     self.timestamp = tstamp
     self.directory_path = None
 
-
-  def get_code(self, base_dir):
-    submission_dir = create_submission_directory(base_dir, self.problem_id, self.submission_id) #method for saving the submitted code into the appropriate file
-    self.directory_path = submission_dir
+  def get_code(self, basedirectory):
+    submissiondirectory = create_submission_directory(basedirectory, self.problem_id, self.submission_id) #method for saving the submitted code into the appropriate file
+    self.directory_path = submissiondirectory
     if self.language.lower() == "python":filename = "solution.py"
     elif self.language.lower() == "c":filename = "solution.c"
     elif self.language.lower() == "cpp":filename = "solution.cpp"
-    elif self.language.lower() == "java":filename = "Solution.java"
     else:raise ValueError("The language is not supported, please try again with a valid option.")
-    file_path = os.path.join(submission_dir, filename)
+    file_path = os.path.join(submissiondirectory, filename)
     with open(file_path, "w") as f:
       f.write(self.code)
     return file_path
-
 
   def metadata(self):
     if self.directory_path is None:
@@ -51,9 +46,8 @@ class submissions:
       f.write(f"submission_id: {self.submission_id}\nuser_id: {self.user_id}\nproblem_id: {self.problem_id}\nlanguage: {self.language}\ntimestamp: {self.timestamp}\ndirectory_path: {self.directory_path}\n")
     return metadata
 
-
-def create_submission_directory(base_dir, problem_id, submission_id): #method for creating a submission folder
-  problem_folder = os.path.join(base_dir, f"problem_{problem_id}")
+def create_submission_directory(basedirectory, problem_id, submission_id): #method for creating a submission folder
+  problem_folder = os.path.join(basedirectory, f"problem_{problem_id}")
   submission_folder = os.path.join(problem_folder, f"submission_{submission_id}")
   os.makedirs(submission_folder, exist_ok=True)
   return submission_folder
